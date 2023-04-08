867848c8844731d075cb941a8c3d6b4c4d74ca4f
Major <major@ycorpo.com>
2023-04-08 12:03:39 +0200

    fix: serve assets

--- _config/nodeAPI/build.sh
+++ _config/nodeAPI/build.sh
@@ -1,6 +1,6 @@
 # bin bash
-mkdir ./next/public
-cp -r ../../1 ./next/public/1
-cp -r ../../10 ./next/public/10
-cp -r ../../250 ./next/public/250
-cp -r ../../42161 ./next/public/42161
+mkdir ./_config/nodeAPI/next/public
+cp -r ../../1 ./_config/nodeAPI/next/public/1
+cp -r ../../10 ./_config/nodeAPI/next/public/10
+cp -r ../../250 ./_config/nodeAPI/next/public/250
+cp -r ../../42161 ./_config/nodeAPI/next/public/42161
