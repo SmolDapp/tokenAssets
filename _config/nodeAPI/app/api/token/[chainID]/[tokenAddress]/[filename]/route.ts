80daeaef2e837d149d149177dce07b04574fbb36
Major <major@ycorpo.com>
2023-04-08 11:25:39 +0200

    fix: lint

--- _config/nodeAPI/app/api/token/[chainID]/[tokenAddress]/[filename]/route.ts
+++ _config/nodeAPI/app/api/token/[chainID]/[tokenAddress]/[filename]/route.ts
@@ -7,23 +7,23 @@ type TContext = {
 		filename: string
 	}
 }
-export async function GET(request: Request, context: TContext) {
-	const chainIDStr = (context?.params?.chainID || 1).toString()
-	const tokenAddress = (context?.params?.tokenAddress || '').toLowerCase()
-	const fileName = (context?.params?.filename || '').toLowerCase()
+export async function GET(request: Request, context: TContext): Promise<Response> {
+	const chainIDStr = (context?.params?.chainID || 1).toString();
+	const tokenAddress = (context?.params?.tokenAddress || '').toLowerCase();
+	const fileName = (context?.params?.filename || '').toLowerCase();
 	if (!['logo.svg', 'logo-32.png', 'logo-128.png'].includes(fileName)) {
-		return new Response('Not found', {status: 404})
+		return new Response('Not found', {status: 404});
 	}
-	console.log(fs.readdirSync(`./`));
+	console.log(fs.readdirSync('./'));
 
-	console.log(fs.readdirSync(`./..`));
+	console.log(fs.readdirSync('./..'));
 
-	console.log(fs.readdirSync(`./../../`));
+	console.log(fs.readdirSync('./../../'));
 
-	const logo = fs.readFileSync(`../../${chainIDStr}/${tokenAddress}/${fileName}`)
+	const logo = fs.readFileSync(`../../${chainIDStr}/${tokenAddress}/${fileName}`);
 
 	if (fileName.endsWith('.svg')) {
-		return new Response(logo, {headers: {'Content-Type': 'image/svg+xml'}})
+		return new Response(logo, {headers: {'Content-Type': 'image/svg+xml'}});
 	}
-	return new Response(logo, {headers: {'Content-Type': 'image/png'}})
+	return new Response(logo, {headers: {'Content-Type': 'image/png'}});
 }
