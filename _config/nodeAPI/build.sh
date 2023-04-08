98f0647731d3fe6ddfad92a751680eb2f6a55873
Major <major@ycorpo.com>
2023-04-08 12:20:48 +0200

    fix: serve assets

--- _config/nodeAPI/build.sh
+++ _config/nodeAPI/build.sh
@@ -5,19 +5,19 @@ mkdir .next/public
 mkdir .next/public/1
 mkdir ./public/1
 cp -rf ../../1 .next/public/1
-cp -rf ../../1 ./public/1
+cp -rf ../../1 ./public/
 
 mkdir .next/public/10
 mkdir ./public/10
 cp -rf ../../10 .next/public/10
-cp -rf ../../10 ./public/10
+cp -rf ../../10 ./public/
 
 mkdir .next/public/250
 mkdir ./public/250
 cp -rf ../../250 .next/public/250
-cp -rf ../../250 ./public/250
+cp -rf ../../250 ./public/
 
 mkdir .next/public/42161
 mkdir ./public/42161
 cp -rf ../../42161 .next/public/42161
-cp -rf ../../42161 ./public/42161
+cp -rf ../../42161 ./public/
