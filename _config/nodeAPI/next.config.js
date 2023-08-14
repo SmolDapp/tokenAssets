3d0bb17db7c566650aad18b3693db07181891956
Major <major@ycorpo.com>
2023-08-14 12:45:41 +0200

    feat: add redirect to github on home access

--- _config/nodeAPI/next.config.js
+++ _config/nodeAPI/next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
 	experimental: {
 		appDir: true,
-	}
+	},
+	redirects() {
+		return [
+			{
+				source: '/',
+				destination: 'https://github.com/SmolDapp/tokenAssets',
+				permanent: true
+			}
+		];
+	},
 }
 
 module.exports = nextConfig
