d77c24116bb628c15e34f6a86897719ebbc177af
Majorfi <major-tom.eth@pm.me>
2023-10-09 13:01:57 +0200

    feat: add chains setup

--- .github/scripts/verify-tokens.mjs
+++ .github/scripts/verify-tokens.mjs
@@ -1,14 +1,14 @@
 import fs from "fs-extra";
 import path from "path";
 
-const DataDirectory = ".";
+const DataDirectory = "./tokens";
 const IndexName = "index.json";
 
 function validate(directory) {
 	let allValid = true;
 	for (let name of fs.readdirSync(directory)) {
 		if (name.startsWith(".") || name === IndexName || name === 'node_modules') continue;
-		let file = path.join(directory, name);
+		const file = path.join(directory, name);
 		const stat = fs.lstatSync(file);
 		if (stat.isDirectory()) {
 			if (name.startsWith("0x")) {
@@ -16,16 +16,6 @@ function validate(directory) {
 					if (name.toLowerCase() !== name) {
 						console.error(`Error: "${name}" is not lowercased. Should be "${name.toLowerCase()}".`);
 						allValid = false;
-						const newName = name.toLowerCase();
-						const newFile = path.join(directory, newName);
-						if (fs.existsSync(newFile)) {
-							console.log(`Deleting existing folder "${newName}"...`);
-							fs.removeSync(newFile);
-						}
-						console.log(`Renaming folder "${name}" to "${newName}"...`);
-						fs.renameSync(file, newFile);
-						name = newName; // Update the name variable to the new lowercase name
-						file = newFile; // Update the file variable to the new folder path
 					}
 				} catch(error) {
 					console.error(`Error: "${name}" is not lowercased. Should be "${name.toLowerCase()}".`);
@@ -33,11 +23,8 @@ function validate(directory) {
 				}
 			}
 			if (name.startsWith("_")) {
-				continue;
+				continue
 			}
-
-			allValid &= validate(file);
-
 			if (name.startsWith("0x")) {
 				if (!fs.existsSync(path.join(file, "logo-128.png"))) {
 					console.error(`Error: "${file}" is missing logo-128.png`);
@@ -47,7 +34,6 @@ function validate(directory) {
 					console.error(`Error: "${file}" is missing logo-32.png`);
 					allValid = false;
 				}
-
 				if (!fs.existsSync(path.join(file, "logo.svg"))) {
 					console.error(`Error: "${file}" is missing logo.svg`);
 					allValid = false;
@@ -59,6 +45,7 @@ function validate(directory) {
 					}
 				}
 			}
+			allValid &= validate(file);
 		}
 	}
 	return allValid;
