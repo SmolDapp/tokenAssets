4b9dacdf774a4a032c7601902339c847c6f3f266
Majorfi <major-tom.eth@pm.me>
2023-10-25 16:36:52 +0200

    fix: delay to 1h

--- _config/nodeAPI/app/api/token/[chainID]/[tokenAddress]/[filename]/route.ts
+++ _config/nodeAPI/app/api/token/[chainID]/[tokenAddress]/[filename]/route.ts
@@ -11,7 +11,7 @@ async function resolveNotFound(request: Request): Promise<Response> {
 	if (fallback === 'true') {
 		const baseURI = (process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : (request as any)?.nextUrl?.origin);
 		const result = await fetch(`${baseURI}/not-found.png`);
-		return new Response(result.body, {headers: {'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=86400, must-revalidate'}});
+		return new Response(result.body, {headers: {'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=3600, must-revalidate'}});
 	}
 
 	if (fallback) {
@@ -19,7 +19,7 @@ async function resolveNotFound(request: Request): Promise<Response> {
 		const contentTypeFromFallback = result.headers.get('Content-Type');
 		if (contentTypeFromFallback?.startsWith('image/')) {
 			console.warn(`Using fallback image for gas token: ${fallback}`);
-			return new Response(result.body, {headers: {'Content-Type': contentTypeFromFallback, 'Cache-Control': 'public, max-age=86400, must-revalidate'}});
+			return new Response(result.body, {headers: {'Content-Type': contentTypeFromFallback, 'Cache-Control': 'public, max-age=3600, must-revalidate'}});
 		}
 	}
 	return new Response('Not found', {status: 404});
@@ -30,14 +30,14 @@ async function resolveGasToken(request: Request): Promise<Response> {
 	if (fallback === 'true') {
 		const baseURI = (process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : (request as any)?.nextUrl?.origin);
 		const result = await fetch(`${baseURI}/gas-token.png`);
-		return new Response(result.body, {headers: {'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=86400, must-revalidate'}});
+		return new Response(result.body, {headers: {'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=3600, must-revalidate'}});
 	}
 	if (fallback) {
 		const result = await fetch(fallback);
 		const contentTypeFromFallback = result.headers.get('Content-Type');
 		if (contentTypeFromFallback?.startsWith('image/')) {
 			console.warn(`Using fallback image for gas token: ${fallback}`);
-			return new Response(result.body, {headers: {'Content-Type': contentTypeFromFallback, 'Cache-Control': 'public, max-age=86400, must-revalidate'}});
+			return new Response(result.body, {headers: {'Content-Type': contentTypeFromFallback, 'Cache-Control': 'public, max-age=3600, must-revalidate'}});
 		}
 	}
 	return new Response('Not found', {status: 404});
@@ -58,9 +58,9 @@ export async function GET(request: Request, context: TContext): Promise<Response
 	const result = await fetch(`${baseURI}/tokens/${chainIDStr}/${tokenAddress}/${fileName}`);
 	if (result.ok) {
 		if (fileName.endsWith('.svg')) {
-			return new Response(result.body, {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'public, max-age=86400, must-revalidate'}});
+			return new Response(result.body, {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'public, max-age=3600, must-revalidate'}});
 		}
-		return new Response(result.body, {headers: {'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=86400, must-revalidate'}});
+		return new Response(result.body, {headers: {'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=3600, must-revalidate'}});
 	}
 
 	if (tokenAddress === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') {
