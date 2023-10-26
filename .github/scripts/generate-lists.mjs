d0f4e0e985dcba511ad1e63bcf63796dfa2d2fe6
Majorfi <major-tom.eth@pm.me>
2023-10-26 10:47:19 +0200

    fix: add versioning

--- .github/scripts/generate-lists.mjs
+++ .github/scripts/generate-lists.mjs
@@ -49,7 +49,43 @@ if (!fs.existsSync(path.join(cwd, ".git"))) {
 try {
 	generate(DataDirectory);
 	for (const chain in perChain) {
-		fs.writeFileSync(path.join(DataDirectory, chain, IndexName), JSON.stringify(perChain[chain], null, 4));
+		//load the existing list
+		const previousLists = fs.existsSync(path.join(DataDirectory, chain, IndexName)) ? JSON.parse(fs.readFileSync(path.join(DataDirectory, chain, IndexName))) : {};
+		if (!previousLists.version) {
+			previousLists.version = {
+				'major': 0,
+				'minor': 0,
+				'patch': 0
+			};
+		}
+		if (!previousLists.tokens) {
+			previousLists.tokens = [];
+		}
+		const newList = {
+			version: previousLists.version,
+			tokens: perChain[chain]
+		}
+		//compare the new list with the old one
+		if (JSON.stringify(previousLists.tokens) === JSON.stringify(newList.tokens)) {
+			console.log(`No changes detected for chain ${chain}`);
+		} else if (previousLists.tokens.length > newList.tokens.length) {
+			// At least one token was removed
+			newList.version.major = previousLists.version.major + 1;
+			newList.version.minor = 0;
+			newList.version.patch = 0;
+		} else if (previousLists.tokens.length < newList.tokens.length) {
+			// At least one token was added
+			newList.version.major = previousLists.version.major;
+			newList.version.minor = previousLists.version.minor + 1;
+			newList.version.patch = 0;
+		} else {
+			// The list has the same length, but at least one token was changed. This should never happen somehown
+			newList.version.major = previousLists.version.major;
+			newList.version.minor = previousLists.version.minor;
+			newList.version.patch = previousLists.version.patch + 1;
+		}
+
+		fs.writeFileSync(path.join(DataDirectory, chain, IndexName), JSON.stringify(newList, null, 4));
 	}
 } catch (error) {
 	console.error(error);
