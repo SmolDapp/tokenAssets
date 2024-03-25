083210aeca3199bc71d05dadeeb1a9ee42c908b5
Majorfi <major@ycorpo.com>
2024-03-25 16:44:23 +0100

    feat: script'n'test

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,13 @@
 import {NextResponse} from 'next/server';
 
+import type {NextRequest} from 'next/server';
+
 export const config = {
 	matcher: '/'
 };
 
-const Middleware = (req) => {
-  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
-    return NextResponse.next();
-
-  return NextResponse.redirect(new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()));
-};
-
-export default Middleware;
+export function middleware(req: NextRequest): NextResponse | Response {
+	//make sure the request is lowercase
+	req.nextUrl.pathname = req.nextUrl.pathname.toLowerCase();
+	return NextResponse.rewrite(req.nextUrl);
+}
