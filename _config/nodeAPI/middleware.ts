1394fc442be656c8c5c000fc3582270c0b294b45
Majorfi <major@ycorpo.com>
2024-03-25 16:57:57 +0100

    feat: script'n'test

--- _config/nodeAPI/middleware.ts
+++ _config/nodeAPI/middleware.ts
@@ -3,11 +3,12 @@ import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
 
 export const config = {
-	matcher: '/'
+	matcher: '/api/token/:chainID/:tokenAddress/:filename'
 };
 
 export function middleware(req: NextRequest): NextResponse | Response {
-	//make sure the request is lowercase
+	const githubBaseURL = 'https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/tokens/';
 	req.nextUrl.pathname = req.nextUrl.pathname.toLowerCase();
-	return NextResponse.rewrite(req.nextUrl);
+	const newURL = new URL(`${githubBaseURL}${req.nextUrl.pathname.replace('/api/token/', '')}`);
+	return NextResponse.redirect(newURL, {status: 308});
 }
