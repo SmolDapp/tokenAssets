1f071b81399e0b641fdfa7c974290f1072f70dfc
Majorfi <major@ycorpo.com>
2024-03-25 17:47:23 +0100

    feat: script'n'test

--- _config/nodeAPI/middleware.ts
+++ _config/nodeAPI/middleware.ts
@@ -7,9 +7,21 @@ export const config = {
 };
 
 export function middleware(req: NextRequest): NextResponse | Response {
-	const githubBaseURL = 'https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/tokens/';
-	const newURL = new URL(`${githubBaseURL}${req.nextUrl.pathname.toLowerCase().replace('/api/token/', '')}`);
-	console.log(newURL);
-	// return NextResponse.next();
+	const githubBaseURL = 'https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/';
+	const pathname = req.nextUrl.pathname.toLowerCase();
+	const splitURL = pathname.split('/');
+	const [/*_api*/, /*_token*/, /*chainID*/, tokenAddress, filename] = splitURL;
+
+	if (!['logo.svg', 'logo-32.png', 'logo-128.png'].includes(filename)) {
+		if (tokenAddress === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') {
+			const newURL = new URL(`${githubBaseURL}_config/nodeAPI/public/gas-token.png`);
+			return NextResponse.redirect(newURL, {status: 308});
+		}
+		const newURL = new URL(`${githubBaseURL}_config/nodeAPI/public/not-found.png`);
+		return NextResponse.redirect(newURL, {status: 308});
+	}
+
+
+	const newURL = new URL(`${githubBaseURL}/tokens/${pathname.replace('/api/token/', '')}`);
 	return NextResponse.redirect(newURL, {status: 308});
 }
