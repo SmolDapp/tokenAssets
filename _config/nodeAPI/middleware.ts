8ec31f03f60dfed45d3c7662ff2cb791c4aefa8d
Majorfi <major@ycorpo.com>
2024-03-25 17:31:08 +0100

    feat: script'n'test

--- _config/nodeAPI/middleware.ts
+++ _config/nodeAPI/middleware.ts
@@ -10,6 +10,6 @@ export function middleware(req: NextRequest): NextResponse | Response {
 	const githubBaseURL = 'https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/tokens/';
 	const newURL = new URL(`${githubBaseURL}${req.nextUrl.pathname.toLowerCase().replace('/api/token/', '')}`);
 	console.log(newURL);
-	return NextResponse.next();
-	// return NextResponse.redirect(newURL, {status: 308});
+	// return NextResponse.next();
+	return NextResponse.redirect(newURL, {status: 308});
 }
