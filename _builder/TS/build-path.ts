36ba46ad65f92045214c901203a91ba71477661a
Dima Rud <dima.rud1995@gmail.com>
2025-03-05 14:43:53 +0200

    fix base64 aryze

--- _builder/TS/build-path.ts
+++ _builder/TS/build-path.ts
@@ -48,35 +48,49 @@ const createTokenFolders = async (tokenConfig: {tokens: IToken[]}, company: stri
 		const tokenDir = path.join(chainDir, token.address.length == 42 ? token.address.toLowerCase() : token.address);
 
 		if (fs.existsSync(tokenDir)) {
-			fs.rmdirSync(tokenDir, {recursive: true});
+			fs.rmSync(tokenDir, {recursive: true});
 		}
 		fs.mkdirSync(tokenDir, {recursive: true});
 
-		const logoUrl = token.logo;
+		const logoUrl_vector = token.logo;
+		const logoUrl =
+			token.logo.includes('sgUSD') || token.logo.includes('sgGOLD')
+				? token.logo
+				: token.logo.replace('_vector.svg', '.svg');
+		console.log(logoUrl_vector);
+
 		const logoExt = extname(logoUrl);
 
-		const logoSvgPath = path.join(tokenDir, 'logo.svg');
+		const logoSvgPath = path.join(tokenDir, 'logo_temp.svg');
+		const logoSvgPath_vector = path.join(tokenDir, 'logo.svg');
 		const logo32Path = path.join(tokenDir, 'logo-32.png');
 		const logo128Path = path.join(tokenDir, 'logo-128.png');
 
 		try {
+			const response_vector = await axios.get(logoUrl_vector, {responseType: 'arraybuffer'});
+			console.log(`Original logo: ${logoSvgPath_vector}`);
+			fs.writeFileSync(logoSvgPath_vector, response_vector.data);
+			//
 			const response = await axios.get(logoUrl, {responseType: 'arraybuffer'});
 			fs.writeFileSync(logoSvgPath, response.data);
-			console.log(`Original logo: ${logoSvgPath}`);
-
+			// console.log(`Original logo: ${logoSvgPath}`);
 			if (logoExt !== '.svg') {
 				// Convert the original logo to PNG format
 				await sharp(logoSvgPath).toFile(logoSvgPath);
 				console.log(`Converted logo: ${logoSvgPath}`);
 				await convertPngToSvgAndResize(logoSvgPath, logoSvgPath, logo32Path, logo128Path);
+				// Delete the original SVG file after converting
 			}
 
+			//
 			// Save the resized logos
 			await sharp(logoSvgPath).resize(128, 128).toFile(logo128Path);
 			console.log(`128px logo: ${logo128Path}`);
 			await sharp(logoSvgPath).resize(32, 32).toFile(logo32Path);
 			console.log(`32px logo: ${logo32Path}`);
-
+			//delete original svg
+			fs.unlinkSync(logoSvgPath);
+			console.log(`Deleted original SVG file: ${logoSvgPath}`);
 			console.log('-'.repeat(80));
 		} catch (error) {
 			console.error(`Failed to download or process logo from URL: ${logoUrl}`, error);
